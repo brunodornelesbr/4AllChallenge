8681e7582c381263954eda79d63d199dc3204f5a
Bruno Dorneles <>
2018-11-19 09:04:16 -0200

    initial commit

---
+++ Code/fourAllChallenge/fourAllChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fourAllChallenge
+//
+//  Created by Bruno Dorneles on 19/11/18.
+//  Copyright © 2018 Bruno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
